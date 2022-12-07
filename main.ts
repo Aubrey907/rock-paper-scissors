34cc2c05e5303f20f7b45e1a9f4249084d17aa9f
Aubrey907 <120053718+Aubrey907@users.noreply.github.com>
2022-12-07 14:11:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(2000)
+        basic.showString("Paper!")
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.pause(2000)
+        basic.showString("Rock!")
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        basic.pause(2000)
+        basic.showString("Scissors!")
+    }
+})
